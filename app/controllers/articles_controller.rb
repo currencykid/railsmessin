494873b75693ef84ca6fd8badfbf63a974a5c917
ck <thecurrencykid@gmail.com>
2016-03-23 19:12:18 -0700

    pagination added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 
 	
 	def index
-		@articles = Article.all.order("created_at DESC")
+		@articles = Article.paginate(page: params[:page], per_page: 20).order("created_at DESC")
 	end 
 
 	def new
