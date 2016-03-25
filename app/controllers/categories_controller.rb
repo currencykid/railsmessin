351776f01367d940bbe04adfc051c5ce30862ae0
ck <thecurrencykid@gmail.com>
2016-03-25 16:50:02 -0700

    admin to create categories test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+	before_action :require_admin, except: [:index, :show] 
+
 	def index
 		@categories = Category.paginate(page: params[:page], per_page: 5).order("name ASC")
 	end
@@ -27,6 +29,15 @@ class CategoriesController < ApplicationController
 		params.require(:category).permit(:name)
 	end 
 
+	private
+
+	def require_admin
+		if !logged_in? || (logged_in? and !current_user.admin?) 
+			flash[:danger] = "Must be an admin to do that" 
+			redirect_to categories_path
+		end 
+	end 
 end
+
 
 
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,20 +1,21 @@
 require 'test_helper' 
 
 class CategoriesControllerTest < ActionController::TestCase
-def setup
- @category = Category.create(name: "sports") #use create instead of new cuz we want to hit db
+	def setup
+		@category = Category.create(name: "sports") #use create instead of new cuz we want to hit db
 
-end 
+		@user = User.create(username: "god" , email: "god@gmail.com", password:"password" , admin: true) 
+	end 
+
 	test "should get categories index" do
 		get :index
 		assert_response :success
 	end
 
 	test "should get new" do
+		session[:user_id] = @user.id 
 		get :new
 		assert_response :success
-
-	
 	end
 
 	test "shoud get show" do
@@ -23,4 +24,10 @@ end
 		assert_response :success
 	end 
 
-end
+	test "should redirect create when not admin" do 
+		assert_no_difference "Category.count" do 
+			post :create, category: {name: "sports"}
+		end 
+		assert_redirected_to categories_path
+	end
+end 	
