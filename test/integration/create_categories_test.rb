c2035c16f871097f3d08e9bf17cfe14614d07381
ck <thecurrencykid@gmail.com>
2016-03-25 16:32:52 -0700

    fixed list & create categories test bug

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -9,7 +9,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 			post_via_redirect categories_path , category: {name:"sports"}
 		end
 		assert_template 'categories/index' 
-#		assert_match "sports", response.body
+		assert_match "sports", response.body
 	end
 
 	test "invalid category submission results in failure" do 
